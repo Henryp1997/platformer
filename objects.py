917797a156310b23ea457d400b4fd06d639ad15f
Henry Pickersgill <htap2@live.co.uk>
2023-12-05 22:09:14 +0000

    trying to get jumping working

--- objects.py
+++ objects.py
@@ -5,16 +5,19 @@ jumping = False
 class player():
     def __init__(self):
         self.x = 100
-        self.y = 600
+        self.y = 500
         self.height = 30
         self.width = 30
         self.speed = 10
+        self.rect = pg.Rect(self.x, self.y, self.width, self.height)
     def move(self):
         key = pg.key.get_pressed()
         if key[pg.K_LEFT]:
-            self.x -= self.speed
+            self.rect.move_ip(self.speed,0)
+            # self.x -= self.speed
         if key[pg.K_RIGHT]:
-            self.x += self.speed
+            self.rect.move_ip(self.speed,1)
+            # self.x += self.speed
         self.draw_rect()
     def jump(self, platform_obj):
         key = pg.key.get_pressed()
@@ -35,9 +38,10 @@ class player():
                 start_jump = False
             self.draw_rect()
         
-
     def check_collisions(self,platform_obj):
         on_platform = False
+        if pg.Rect(self.x, self.y, self.width, self.height).colliderect(pg.Rect(platform_obj.x, platform_obj.y, platform_obj.width, platform_obj.height)):
+            print("yes")
         if (self.y + self.height == platform_obj.y) and (platform_obj.x <= self.x + self.width and self.x <= platform_obj.x + platform_obj.width):
             on_platform = True
         if round(self.y + self.height) > screen_y - 10:
