05589cbe0226feb72d5f23ed89d6231cac4e9679
Henry Pickersgill <hpickersgill@crl.toshiba.co.uk>
2023-12-30 20:28:30 +0000

    platform collision working with one platform

--- objects.py
+++ objects.py
@@ -5,50 +5,77 @@ jumping = False
 class player():
     def __init__(self):
         self.x = 100
-        self.y = 500
+        self.y = 200
         self.height = 30
         self.width = 30
         self.speed = 10
+        self.gravity = 5
         self.rect = pg.Rect(self.x, self.y, self.width, self.height)
-    def move(self):
+
+    def move(self, all_platforms):
         key = pg.key.get_pressed()
         if key[pg.K_LEFT]:
-            self.rect.move_ip(self.speed,0)
-            # self.x -= self.speed
+            self.rect.move_ip(self.speed, 0)
+            self.x -= self.speed
         if key[pg.K_RIGHT]:
-            self.rect.move_ip(self.speed,1)
-            # self.x += self.speed
+            self.rect.move_ip(self.speed, 1)
+            self.x += self.speed
+        
+        for platform_obj in all_platforms:
+            player_below_platform = self.y + self.height > platform_obj.y
+            player_about_to_land = self.y + self.height + self.gravity >= platform_obj.y
+            player_at_right_edge = self.x > platform_obj.x + platform_obj.width
+            player_at_left_edge = self.x + self.width < platform_obj.x
+
+            # start falling if not on platform
+            if player_at_right_edge or player_at_left_edge:
+                self.gravity = 5
+
+            # player on platform
+            if self.gravity == 0:
+                # snap player to platform
+                self.y = platform_obj.y - self.height
+            
+            self.y += self.gravity
+            if player_about_to_land and not player_below_platform:
+                # if the player is going to be inside the platform on the next frame
+                self.gravity = 0
+
         self.draw_rect()
-    def jump(self, platform_obj):
-        key = pg.key.get_pressed()
-        start_jump = False
-        if key[pg.K_SPACE]:
-            on_platform = self.check_collisions(platform_obj)
-            if on_platform:
-                start_jump = True
+
+    # def jump(self, platform_obj):
+    #     key = pg.key.get_pressed()
+    #     start_jump = False
+    #     if key[pg.K_SPACE]:
+    #         on_platform = self.check_collisions(platform_obj)
+    #         if on_platform:
+    #             start_jump = True
         
-        original_y = self.y
-        negate_speed = 1
-        if start_jump:
-            self.y -= negate_speed * 10
-            if self.y < original_y - 50:
-                negate_speed = -1
-            on_platform = self.check_collisions(platform_obj)
-            if on_platform:
-                start_jump = False
-            self.draw_rect()
+    #     original_y = self.y
+    #     negate_speed = 1
+    #     if start_jump:
+    #         self.y -= negate_speed * 10
+    #         if self.y < original_y - 50:
+    #             negate_speed = -1
+    #         on_platform = self.check_collisions(platform_obj)
+    #         if on_platform:
+    #             start_jump = False
+    #         self.draw_rect()
         
-    def check_collisions(self,platform_obj):
-        on_platform = False
-        if pg.Rect(self.x, self.y, self.width, self.height).colliderect(pg.Rect(platform_obj.x, platform_obj.y, platform_obj.width, platform_obj.height)):
-            print("yes")
-        if (self.y + self.height == platform_obj.y) and (platform_obj.x <= self.x + self.width and self.x <= platform_obj.x + platform_obj.width):
-            on_platform = True
-        if round(self.y + self.height) > screen_y - 10:
-            on_platform = True
-        if not on_platform:
-            self.y += 10
-        return on_platform
+    # def check_collisions(self, platform_obj):
+        # on_platform = False
+        # if pg.Rect(self.x, self.y, self.width, self.height).colliderect(pg.Rect(platform_obj.x, platform_obj.y, platform_obj.width, platform_obj.height)):
+        #     print("yes")
+        # if (self.y + self.height == platform_obj.y) and (platform_obj.x <= self.x + self.width and self.x <= platform_obj.x + platform_obj.width):
+        #     on_platform = True
+        # if round(self.y + self.height) > screen_y - 10:
+        #     on_platform = True
+        # if not on_platform:
+        #     self.y += 10
+        # return on_platform
+
+
+        # check whether the player is going to be inside the platform on the next frame. If so, register a collision
 
 
     
--- platformer.py
+++ platformer.py
@@ -5,27 +5,30 @@ from variables import *
 from objects import *
 
 player_ = player()
-platform_ = platform(80, 630, 100, 20)
+all_platforms = [platform(80, 350, 100, 20)]
 frame_count = 0
-frame_counts = [0,60]
+frame_counts = [0, 60]
 jumping = False
-# game code
-while True:
-    clock = pg.time.Clock()
-    clock.tick(60)
-    screen.fill(colours['BLACK'])
-    pg.draw.rect(screen, colours['GREY1'], pg.Rect((0,0), (screen_x, screen_y)), width=5)
-    player_.move()
-    platform_.draw_rect()
 
-    player_.check_collisions(platform_)
-    player_.jump(platform_) 
+def main():
+    # game code
+    while True:
+        clock = pg.time.Clock()
+        clock.tick(60)
+        screen.fill(colours['BLACK'])
+        pg.draw.rect(screen, colours['GREY1'], pg.Rect((0,0), (screen_x, screen_y)), width=5)
+        player_.move(all_platforms)
 
-    pg.display.update()
-    for event in pg.event.get():
-        if event.type == QUIT:
-            pg.quit()
-            sys.exit()
-    frame_count += 1
-    frame_counts[1] = frame_count
-    
+        for platform_obj in all_platforms:
+            platform_obj.draw_rect()
+
+        # player_.check_collisions(platform_)
+        # player_.jump(platform_) 
+
+        pg.display.update()
+        for event in pg.event.get():
+            if event.type == QUIT:
+                pg.quit()
+                sys.exit()
+
+main()
